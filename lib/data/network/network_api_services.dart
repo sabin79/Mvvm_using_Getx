492f4b736c67cbc3766a0d30089760466ecb22c5
sabin79 <dsabin729@gmail.com>
2023-12-28 20:50:44 +0545

    Network Services Api

--- lib/data/network/network_api_services.dart
+++ lib/data/network/network_api_services.dart
@@ -29,10 +29,18 @@ class NetworkApiServices extends BaseApiServices {
 
   @override
   Future<dynamic> postApi(var data, String url) async {
+    if (kDebugMode) {
+      print(url);
+      print(data);
+    }
     dynamic responseJson;
     try {
-      final response =
-          await http.get(Uri.parse(url)).timeout(const Duration(seconds: 10));
+      final response = await http
+          .post(Uri.parse(url),
+              body: jsonEncode(data) //when data is from raw from
+              //body : data (when data is not in raw from) formeddata
+              )
+          .timeout(const Duration(seconds: 10));
       responseJson = returnResponse(response);
     } on SocketException {
       throw InternetException();
